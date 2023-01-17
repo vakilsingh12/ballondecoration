c7ad1dfca847adde92dd0420a8cb6e18f0ff87c2
Vakil.Verma <vakil@nimapinfotech.com>
2023-01-17 09:12:00 +0530

    about page updated

--- pages/component/About.js
+++ pages/component/About.js
@@ -10,27 +10,7 @@ const About = () => {
         </h2>
         <div className="flex sm:flex-row  items-start mx-auto md:py-4">
           <p className="flex-grow text-gray-900 mt-10 text-justify md:mx-12">
-            About A1 Balloon A1 Balloon is the leading balloon decoration company
-            in Gurgaon. We provide a wide range of decoration services to make
-            any event full of joy, happiness and memorable. We have different
-            types of packages for every event. The decoration is done by a team
-            of professionals keeping an eye on each corner of the place to be
-            well decorated. Our team convert any place into the best-decorated
-            place fulfilling all the decoration needs of the event. Our
-            decoration experts use their unique idea to make every event
-            memorable and enjoyable. We are a team of event and party
-            decorators, very well versed in the different types of decoration
-            styles. Our experts are well trained and experienced in managing and
-            decorating big events. Our luxury balloon decoration services cover
-            all types of events and needs of clients such as baby showers,
-            welcome baby decoration, corporate event decoration, party
-            decoration, wedding decoration, birthday decoration, bachelorette
-            party decoration, anniversary decoration, office party decoration
-            and more. We use modern items and methods for decoration giving the
-            unique and beautiful design that makes A1 Balloon Decoration a
-            one-stop solution for decoration services in Gurgaon. If you are
-            looking for balloon decoration near me in Gurgaon, A1 Balloon is the
-            best service provider for your event decoration need.
+            One does not remember days, but remembers moments. We understand the value of an occasion and the emotions attached to it. We provide a wide range of decoration services to make any event full of joy, happiness and memorable. Our luxury balloon decoration services cover all types of events and needs of clients such as baby showers, welcome baby decoration, corporate event decoration, party decoration, wedding decoration, birthday decoration, bachelorette party decoration, anniversary decoration, office party decoration and more.
           </p>
         </div>
       </div>
