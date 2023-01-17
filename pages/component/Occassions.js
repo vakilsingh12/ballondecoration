bd66467e323d56b15b505f477eb1582b59047cd4
Vakil.Verma <vakil@nimapinfotech.com>
2023-01-17 10:13:05 +0530

    header updated

--- pages/component/Occassions.js
+++ pages/component/Occassions.js
@@ -3,10 +3,15 @@ import Image from "next/image";
 const Occassions = () => {
   return (
     <>
-      <div className="flex min-h-full items-center justify-center md:py-10 py-5 px-4 sm:px-6 lg:px-8" id="occasion">
+      <div
+        className="flex min-h-full items-center justify-center md:py-10 py-5 px-4 sm:px-6 lg:px-8"
+        id="occasion"
+      >
         <div className="w-full max-w-md space-y-8">
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight">
-            <span className="pb-4 text-gray-900 border-b-4 border-gray-800">Occasions</span>
+            <span className="pb-4 text-gray-900 border-b-4 border-gray-800">
+              Occasions
+            </span>
           </h2>
         </div>
       </div>
@@ -14,13 +19,13 @@ const Occassions = () => {
         <div className="container px-5 md:py-20 py-10 mx-auto">
           <div className="flex flex-wrap -m-4 md:mx-10 mx-20 sm:items-center sm:justify-center">
             <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src='/kidsbirthday.jpg'
-                  width={200}
-                  height={200}
-                />
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="/kidsbirthday.jpg"
+                width={200}
+                height={200}
+              />
               <div className="mt-4">
                 <h3 className="text-gray-500 mb-1 ml-8">
                   Kid's Birthday Party
@@ -28,157 +33,137 @@ const Occassions = () => {
               </div>
             </div>
             <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src="https://cdn.shopify.com/s/files/1/0266/9497/5657/products/happy-anniversary-decoration-kit-with-golden-foil-balloon-pack-of-84-pcs-330839_533x.jpg?v=1646290662"
-                  width={200}
-                  height={200}
-                />
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="/adult.jpg"
+                width={200}
+                height={200}
+              />
               <div className="mt-4">
-                <h3 className="text-gray-500 mb-1 ml-8">
-                  Anniversary party
-                </h3>
+                <h3 className="text-gray-500 mb-1 ml-12">Adult's Birthday</h3>
               </div>
             </div>
             <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src='/occ1.jpg'
-                  width={200}
-                  height={200}
-                />
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="https://cdn.shopify.com/s/files/1/0266/9497/5657/products/happy-anniversary-decoration-kit-with-golden-foil-balloon-pack-of-84-pcs-330839_533x.jpg?v=1646290662"
+                width={200}
+                height={200}
+              />
               <div className="mt-4">
-                <h3 className="text-gray-500 mb-1 ml-12">
-                  Office Party
-                </h3>
+                <h3 className="text-gray-500 mb-1 ml-8">Anniversary party</h3>
               </div>
             </div>
             <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src='/occ2.jpg'
-                  width={200}
-                  height={200}
-                />
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="/occ1.jpg"
+                width={200}
+                height={200}
+              />
               <div className="mt-4">
-                <h3 className="text-gray-500 mb-1 ml-16">
-                Wedding
-                </h3>
+                <h3 className="text-gray-500 mb-1 ml-12">Office Party</h3>
               </div>
             </div>
             <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src='/occ3.jpg'
-                  width={200}
-                  height={200}
-                />
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="/occ2.jpg"
+                width={200}
+                height={200}
+              />
               <div className="mt-4">
-                <h3 className="text-gray-500 mb-1 ml-8">
-                Bachelorette Party
-                </h3>
+                <h3 className="text-gray-500 mb-1 ml-16">Wedding</h3>
               </div>
             </div>
             <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src='/occ4.jpg'
-                  width={200}
-                  height={200}
-                />
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="/occ3.jpg"
+                width={200}
+                height={200}
+              />
               <div className="mt-4">
-                <h3 className="text-gray-500 mb-1 ml-8">
-                Retirement Party
-                </h3>
+                <h3 className="text-gray-500 mb-1 ml-8">Bachelorette Party</h3>
               </div>
             </div>
             <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src='/vel.jpg'
-                  width={200}
-                  height={200}
-                />
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="/occ4.jpg"
+                width={200}
+                height={200}
+              />
               <div className="mt-4">
-                <h3 className="text-gray-500 mb-1 ml-10">
-                Valentine's Day
-                </h3>
+                <h3 className="text-gray-500 mb-1 ml-8">Retirement Party</h3>
               </div>
             </div>
             <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src='/chris.jpg'
-                  width={200}
-                  height={200}
-                />
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="/vel.jpg"
+                width={200}
+                height={200}
+              />
+              <div className="mt-4">
+                <h3 className="text-gray-500 mb-1 ml-10">Valentine's Day</h3>
+              </div>
+            </div>
+            <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="/chris.jpg"
+                width={200}
+                height={200}
+              />
               <div className="mt-4">
                 <h3 className="text-gray-500 mb-1 ml-6">
-                Christmas & New Year
+                  Christmas & New Year
                 </h3>
               </div>
             </div>
             <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src='/baby.jpg'
-                  width={200}
-                  height={200}
-                />
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="/baby.jpg"
+                width={200}
+                height={200}
+              />
               <div className="mt-4">
-                <h3 className="text-gray-500 mb-1 ml-12">
-                Baby Shower
-                </h3>
+                <h3 className="text-gray-500 mb-1 ml-12">Baby Shower</h3>
               </div>
             </div>
             <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src='/school.jpg'
-                  width={200}
-                  height={200}
-                />
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="/school.jpg"
+                width={200}
+                height={200}
+              />
               <div className="mt-4">
-                <h3 className="text-gray-500 mb-1 ml-12">
-                School Function
-                </h3>
+                <h3 className="text-gray-500 mb-1 ml-12">School Function</h3>
               </div>
             </div>
             <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src='/adult.jpg'
-                  width={200}
-                  height={200}
-                />
+              <img
+                alt="balloondecoration"
+                className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
+                src="/babya.jpg"
+                width={200}
+                height={200}
+              />
               <div className="mt-4">
-                <h3 className="text-gray-500 mb-1 ml-12">
-                Adult's Birthday
-                </h3>
-              </div>
-            </div>
-            <div className="lg:w-1/6 md:w-1/2 p-2 w-full">
-                <img
-                  alt="balloondecoration"
-                  className="shadow-lg shadow-pink-400 rounded-full transition duration-500 ease hover:-translate-y-3 hover:cursor-pointer"
-                  src='/babya.jpg'
-                  width={200}
-                  height={200}
-                />
-              <div className="mt-4">
-                <h3 className="text-gray-500 mb-1 ml-14">
-                Baby Arrival
-                </h3>
+                <h3 className="text-gray-500 mb-1 ml-14">Baby Arrival</h3>
               </div>
             </div>
           </div>
