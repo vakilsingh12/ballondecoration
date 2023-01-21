eabc504b63bbaf32f12fef4e0e44e749c62de082
Vakil.Verma <vakil@nimapinfotech.com>
2023-01-21 17:41:49 +0530

    service and occasion changes updated

--- pages/component/Footer.js
+++ pages/component/Footer.js
@@ -67,7 +67,7 @@ const Footer = () => {
             transition
             ease-in-out
             m-0
-            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput7"
+            focus:text-gray-700 focus:bg-white focus:border-fuchsia-600 focus:outline-none" id="exampleInput7"
               placeholder="Name"/>
           </div>
           <div className="form-group mb-4">
@@ -84,7 +84,7 @@ const Footer = () => {
             transition
             ease-in-out
             m-0
-            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleInput8"
+            focus:text-gray-700 focus:bg-white focus:border-fuchsia-600 focus:outline-none" id="exampleInput8"
               placeholder="Email address"/>
           </div>
           <div className="form-group mb-4">
@@ -103,14 +103,14 @@ const Footer = () => {
             transition
             ease-in-out
             m-0
-            focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+            focus:text-gray-700 focus:bg-white focus:border-fuchsia-600 focus:outline-none
           " id="exampleFormControlTextarea13" rows="3" placeholder="Message"></textarea>
           </div>
           <button type="submit" className="
           w-full
           px-6
           py-2.5
-          bg-blue-600
+          bg-fuchsia-600
           text-white
           font-medium
           text-xs
@@ -118,9 +118,9 @@ const Footer = () => {
           uppercase
           rounded
           shadow-md
-          hover:bg-blue-700 hover:shadow-lg
-          focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-          active:bg-blue-800 active:shadow-lg
+          hover:bg-fuchsia-700 hover:shadow-lg
+          focus:bg-fuchsia-700 focus:shadow-lg focus:outline-none focus:ring-0
+          active:bg-fuchsia-800 active:shadow-lg
           transition
           duration-150
           ease-in-out">Send</button>
