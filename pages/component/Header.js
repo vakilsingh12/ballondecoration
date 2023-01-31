ad44e923302379d89929ab0dcd77d60bf5bdb313
Vakil.Verma <vakil@nimapinfotech.com>
2023-01-31 12:16:59 +0530

    email changed

--- pages/component/Header.js
+++ pages/component/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
         </div>
         </div>
         <div className="flex items-center px-6 text-white">
-           <AiOutlineMail/> : a2zballoondecoration@gmail.com
+           <AiOutlineMail/> : a2zballoondecor@gmail.com
         </div>
       </div>
       <nav className="bg-white border-gray-200 px-2 sm:px-4 py-1 md:shadow-lg shadow-sm rounded dark:bg-gray-900">
