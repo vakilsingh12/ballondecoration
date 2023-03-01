a9a5af1160dea37c00ae96503f8e1add23d8461b
Vakil.Verma <vakil@nimapinfotech.com>
2023-03-01 16:33:41 +0530

    image size updated in gallery

--- pages/component/Gallery.js
+++ pages/component/Gallery.js
@@ -11,75 +11,85 @@ const Gallery = () => {
       <section className="overflow-hidden text-gray-700 ">
         <div className="container px-5 py-4 mx-auto lg:pt-12 lg:px-12">
           <div className="flex flex-wrap -m-1 md:-m-2">
-            <div className="flex flex-wrap w-1/4">
+          
+            <div className="flex flex-wrap w-96">
               <div className="w-full p-1 md:p-2">
                 <img
                   alt="gallery"
-                  className="block object-cover object-center w-full md:h-72 h-20 rounded-lg"
+                  className="block object-cover object-center w-full md:h-96 h-44 rounded-lg"
                   src="https://rukminim1.flixcart.com/image/850/850/l48s9zk0/balloon/j/7/3/3-5-haldi-ceremony-helium-balloon-decoration-de4-gorgeous-moment-original-imagf6ejz5f7yzds.jpeg?q=90"
                 />
               </div>
             </div>
-            <div className="flex flex-wrap w-1/4">
+            <div className="flex flex-wrap w-96">
               <div className="w-full p-1 md:p-2">
                 <img
                   alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
+                  className="block object-cover object-center w-full md:h-full h-44 rounded-lg"
                   src="/gal10.jpg"
                 />
               </div>
             </div>
-            <div className="flex flex-wrap w-1/4">
+            <div className="flex flex-wrap w-96">
               <div className="w-full p-1 md:p-2">
                 <img
                   alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
+                  className="block object-cover object-center w-full md:h-full h-44 rounded-lg"
                   src="/gal3.jpg"
                 />
               </div>
             </div>
-            <div className="flex flex-wrap w-1/4 ">
+            <div className="flex flex-wrap w-96 ">
               <div className="w-full p-1 md:p-2">
                 <img
                   alt="gallery"
-                  className="block object-cover object-center w-full md:h-72 h-20 rounded-lg  "
+                  className="block object-cover object-center w-full md:h-96 h-44 rounded-lg  "
                   src="/car.jpg"
                 />
               </div>
             </div>
-            <div className="flex flex-wrap w-1/4">
+            <div className="flex flex-wrap w-96">
               <div className="w-full p-1 md:p-2">
                 <img
                   alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
+                  className="block object-cover object-center w-full md:h-full h-44 rounded-lg"
                   src="/gal5.jpg"
                 />
               </div>
             </div>
-            <div className="flex flex-wrap w-1/4">
+            <div className="flex flex-wrap w-96">
               <div className="w-full p-1 md:p-2">
                 <img
                   alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
+                  className="block object-cover object-center w-full md:h-full h-44 rounded-lg"
                   src="/gal6.jpg"
                 />
               </div>
             </div>
-            <div className="flex flex-wrap w-1/4">
+            <div className="flex flex-wrap w-96">
               <div className="w-full p-1 md:p-2">
                 <img
                   alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
+                  className="block object-cover object-center w-full md:h-full h-44 rounded-lg"
                   src="/main1.jpg"
                 />
               </div>
             </div>
-            <div className="flex flex-wrap w-1/4">
+            <div className="flex flex-wrap w-96">
               <div className="w-full p-1 md:p-2">
                 <img
                   alt="gallery"
-                  className="block object-cover object-center w-full h-full rounded-lg"
+                  className="block object-cover object-center w-full md:h-full h-44 rounded-lg"
                   src="/gal1.jpg"
+                />
+              </div>
+            </div>
+            <div className="flex flex-wrap w-96">
+              <div className="w-full p-1 md:p-2">
+                <img
+                  alt="gallery"
+                  src="https://deowgxgt4vwfe.cloudfront.net/uploads/1646830152_original.jpg"
+                  className="block object-cover object-center w-full md:h-full h-44 rounded-lg" 
                 />
               </div>
             </div>
